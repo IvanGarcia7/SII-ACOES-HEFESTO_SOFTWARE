f1a5a8c9ffb802904fe105f99025c293c0fa40f7
Iván Garcia <ivanbolero@hotmail.com>
2019-04-04 16:09:28 +0200

    Commit Final

--- HefestoSoftware/build/generated-sources/ap-source-output/hefestosoftware/HistorialPaquetes_.java
+++ HefestoSoftware/build/generated-sources/ap-source-output/hefestosoftware/HistorialPaquetes_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-04T14:37:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-04T16:02:21")
 @StaticMetamodel(HistorialPaquetes.class)
 public class HistorialPaquetes_ { 
 
--- HefestoSoftware/build/generated-sources/ap-source-output/hefestosoftware/Usuario_.java
+++ HefestoSoftware/build/generated-sources/ap-source-output/hefestosoftware/Usuario_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-04T14:37:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-04T16:02:21")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
