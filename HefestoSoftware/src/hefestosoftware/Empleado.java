1f48916b4734d73f64d651dddb1588c212f2159e
AmbroxMr <37145368+AmbroxMr@users.noreply.github.com>
2019-04-04 15:03:44 +0200

    Update Empleado.java

--- HefestoSoftware/src/hefestosoftware/Empleado.java
+++ HefestoSoftware/src/hefestosoftware/Empleado.java
@@ -184,7 +184,6 @@ public class Empleado implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof Empleado)) {
             return false;
         }
