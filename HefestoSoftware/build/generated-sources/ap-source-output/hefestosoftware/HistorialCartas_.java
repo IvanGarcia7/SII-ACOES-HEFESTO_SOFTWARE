f6420f8163a44437542df83dc973ee20b31e9c40
Iván Garcia <ivanbolero@hotmail.com>
2019-04-04 14:14:39 +0200

    Nuevo Commit

--- HefestoSoftware/build/generated-sources/ap-source-output/hefestosoftware/HistorialCartas_.java
+++ HefestoSoftware/build/generated-sources/ap-source-output/hefestosoftware/HistorialCartas_.java
@@ -1,17 +1,15 @@
 package hefestosoftware;
 
-import hefestosoftware.Carta;
 import hefestosoftware.Usuario;
 import java.sql.Date;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-04T13:55:58")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-04T14:07:15")
 @StaticMetamodel(HistorialCartas.class)
 public class HistorialCartas_ { 
 
-    public static volatile SingularAttribute<HistorialCartas, Carta> carta;
     public static volatile SingularAttribute<HistorialCartas, Boolean> estado;
     public static volatile SingularAttribute<HistorialCartas, Date> fechaEnvio;
     public static volatile SingularAttribute<HistorialCartas, Usuario> usuario;
--- HefestoSoftware/build/generated-sources/ap-source-output/hefestosoftware/Peticion_.java
+++ HefestoSoftware/build/generated-sources/ap-source-output/hefestosoftware/Peticion_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-04T13:55:58")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-04T14:07:15")
 @StaticMetamodel(Peticion.class)
 public class Peticion_ { 
 
