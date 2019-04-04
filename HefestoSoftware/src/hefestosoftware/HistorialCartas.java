1c0d360202133707d581b903f78f65f1457896b1
AmbroxMr <37145368+AmbroxMr@users.noreply.github.com>
2019-04-04 15:04:12 +0200

    Update HistorialCartas.java

--- HefestoSoftware/src/hefestosoftware/HistorialCartas.java
+++ HefestoSoftware/src/hefestosoftware/HistorialCartas.java
@@ -81,7 +81,6 @@ public class HistorialCartas implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof HistorialCartas)) {
             return false;
         }
