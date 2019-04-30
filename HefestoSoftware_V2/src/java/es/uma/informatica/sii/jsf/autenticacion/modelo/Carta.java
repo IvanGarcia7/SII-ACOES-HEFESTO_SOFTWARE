bf6b76359bc3e9dbb5a761cde9148ff134e3ac70
zomakus <38212424+zomakus@users.noreply.github.com>
2019-04-30 13:18:16 +0200

    Add files via upload

--- HefestoSoftware_V2/src/java/es/uma/informatica/sii/jsf/autenticacion/modelo/Carta.java
+++ HefestoSoftware_V2/src/java/es/uma/informatica/sii/jsf/autenticacion/modelo/Carta.java
@@ -21,7 +21,7 @@ import javax.persistence.ManyToOne;
  */
 @Entity
 public class Carta implements Serializable {
-
+    
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
