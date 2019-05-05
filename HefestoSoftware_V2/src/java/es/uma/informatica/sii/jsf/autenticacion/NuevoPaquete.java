d84fc9101f6bbe979b76eaf66074b08d03f902ae
Iván Garcia <ivanbolero@hotmail.com>
2019-05-05 14:48:27 +0200

    Arreglos

---
+++ HefestoSoftware_V2/src/java/es/uma/informatica/sii/jsf/autenticacion/NuevoPaquete.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.uma.informatica.sii.jsf.autenticacion;
+
+
+import es.uma.informatica.sii.jsf.autenticacion.modelo.Paquete;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.sql.Date;
+
+/**
+ *
+ * 
+ */
+@Named(value="nuevoPaquete")
+@RequestScoped
+public class NuevoPaquete {
+    private Paquete paquete;
+    
+    @Inject
+    private ServicioPaquetes servicio;
+    @Inject
+    private ControlAutorizacionAdministracion ctrl;
+    
+    public NuevoPaquete(){
+        paquete = new Paquete();
+    }
+    
+    public void enviarCarta(){
+        
+        paquete.setFechaEnvio(new Date(System.currentTimeMillis()));
+        servicio.añadirPaquete(paquete);
+    }
+
+    public Paquete getPaquete() {
+        return paquete;
+    }
+
+    public void setPaquete(Paquete paquete) {
+        this.paquete = paquete;
+    }
+
+    public ServicioPaquetes getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(ServicioPaquetes servicio) {
+        this.servicio = servicio;
+    }
+
+    public ControlAutorizacionAdministracion getCtrl() {
+        return ctrl;
+    }
+
+    public void setCtrl(ControlAutorizacionAdministracion ctrl) {
+        this.ctrl = ctrl;
+    }
+    
+}
