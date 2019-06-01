69b20950c63a2511f758721dc25e765c8324d9c4
Iván Garcia <ivanbolero@hotmail.com>
2019-06-01 11:37:21 +0200

    fix

--- ACOES-HS/ACOES-HS-ejb/build/generated-sources/ap-source-output/es/uma/informatica/sii/hs/entidades/HistorialPadrinos_.java
+++ ACOES-HS/ACOES-HS-ejb/build/generated-sources/ap-source-output/es/uma/informatica/sii/hs/entidades/HistorialPadrinos_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-06-01T10:45:38")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-06-01T11:20:11")
 @StaticMetamodel(HistorialPadrinos.class)
 public class HistorialPadrinos_ { 
 
